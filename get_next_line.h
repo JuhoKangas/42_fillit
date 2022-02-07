a7ce93647f21f5e94c1e46a8a9991f25b6d4713a
Juho Kangas <jkangas@student.hive.fi>
2022-02-07 16:18:11 +0200

    add get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkangas <jkangas@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/13 17:04:55 by jkangas           #+#    #+#             */
+/*   Updated: 2022/01/19 17:06:00 by jkangas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 32
+# define FD_SIZE 5000
+
+int	get_next_line(const int fd, char **line);
+
+#endif
